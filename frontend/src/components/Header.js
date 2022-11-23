3470cd5a544e3d6d69c923596d6cc009ceb7027b
Sergio Garcia <segarciat314@gmail.com>
2022-11-22 22:03:33 -0500

    Created Header with a Navbar

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Navbar, Container} from 'react-bootstrap';
+
+const Header = ({title}) => {
+    return (
+        <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt="Recipedia logo"
+              src="logo.png"
+              className="d-inline-block align-top"
+            />{' '}
+            {title}
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    )
+}
+
+export default Header;
