9aad0ae779ea521b93739182bd1e6d4ffe25736a
Sergio Garcia <segarciat314@gmail.com>
2022-11-22 23:06:20 -0500

    Made Search a controlled component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
 function App() {
+  const [query, setQuery] = useState("");
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+  };
+
   return (
     <div>
       <Header title="Recipedia" />
-      <Search />
+      <Search
+        query={query}
+        setQuery={setQuery}
+        handleSubmit={handleSearchSubmit}
+      />
     </div>
   );
 }
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Container, Form, Row, Col, Button } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({ query, setQuery, handleSubmit }) => {
   return (
     <Container className="mt-4">
       <Row>
         <Col xs={12} md={8}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Row>
               <Col xs={9}>
-                <Form.Control placeholder="Search recipes..." />
+                <Form.Control
+                  type="text"
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                  placeholder="Search..."
+                />
               </Col>
               <Col>
                 <Button variant="secondary" type="submit">
