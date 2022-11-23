60ad6b134881d464ee3ee581431e5498f3dba242
Sergio Garcia <segarciat314@gmail.com>
2022-11-22 23:49:00 -0500

    Packages to integrate eslint and prettier, and prettier config in package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
-import Search from "./components/Search";
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/Search';
 
 function App() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(e.target[0].value);
     // Clear search input.
-    setQuery("");
+    setQuery('');
   };
 
   return (
