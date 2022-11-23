f81b670805918b788b785b17b5c5f7a2e915ffa7
Sergio Garcia <segarciat314@gmail.com>
2022-11-23 00:42:29 -0500

    Displaying recipe cards from hard-coded array

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
+
+const recipes = [
+  { title: 'Brownies', description: 'Fudgy inside, cakey outside' },
+  { title: 'Lasagna', description: 'Cheesy and saucy' },
+  { title: 'Tteokbokki', description: 'Rice cakes with gochujang' },
+  { title: 'Beyond Burger', description: 'Meatless burger' },
+];
 
 function App() {
   const [query, setQuery] = useState('');
@@ -21,6 +30,15 @@ function App() {
         setQuery={setQuery}
         handleSubmit={handleSearchSubmit}
       />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {recipes.map((recipe, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard {...recipe} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
